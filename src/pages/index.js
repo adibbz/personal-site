b77671a3bd497fc8690fa3412835a419c338f0b7
Andrew Dibb <andrew@singlemusic.com>
2020-11-23 11:33:56 -0600

    home page updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,15 @@ class IndexPage extends React.Component {
             <Layout>
              <Helmet title="Andrew Dibb | Home"></Helmet>
                 <div className="home-wrapper">
-                    <div id="main">
-                        <ScrollAnimation className="content" animateIn="fadeIn" animateOnce={true} duration={2}>
-                            <h1>Andrew Dibb</h1>
-                            <hr className="line" />
-                            <h4>I am a front end developer</h4>
-                            <SocialList />
-                        </ScrollAnimation>
+                    <div className="container">
+                        <div id="main">
+                            <ScrollAnimation className="content" animateIn="fadeIn" animateOnce={true} duration={2}>
+                                <h1>Andrew Dibb</h1>
+                                <hr className="line" />
+                                <h4>I am a front end developer</h4>
+                                <SocialList />
+                            </ScrollAnimation>
+                        </div>
                     </div>
                 </div>
             </Layout>
