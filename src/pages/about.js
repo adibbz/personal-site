596d1e6edead022341048128396c432eaf01a22e
Andrew Dibb <andrew@singlemusic.com>
2022-05-26 14:28:46 -0500

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ class AboutIndex extends React.Component {
             <ScrollAnimation className="content" animateIn="fadeIn" animateOnce={true} duration={2}>
               <h1>About</h1>
               <hr className="line" />
-              <p>Hello! I am Andrew Dibb, a front end developer from Nashville, TN, who is passionate about all things front end web. I'm  a self-starter who is very enthusiastic about learning and have a strong work ethic. <a href="mailto:andrewdibb@gmail.com">Email</a> me to talk about working together or just say hello!</p>
+              <p>Hello! I am Andrew Dibb, a UX engineer from Nashville, TN, who is passionate about all things front end web. I'm  a self-starter who is very enthusiastic about learning and have a strong work ethic. <a href="mailto:andrewdibb@gmail.com">Email</a> me to talk about working together or just say hello!</p>
               <h5>Things I use:</h5>
               <ul className="cols">
                 <li>Angular</li>
@@ -22,10 +22,9 @@ class AboutIndex extends React.Component {
                 <li>React</li>
                 <li>Ionic</li>
                 <li>React Native</li>
-                <li>.NET</li>
-                <li>Wordpress</li>
-                <li>Foundation/<br></br>Bootstrap</li>
+                <li>Bootstrap</li>
                 <li>Git</li>
+                <li>Figma</li>
                 <li>Sketch</li>
                 <li>Photoshop</li>
               </ul>
@@ -34,9 +33,10 @@ class AboutIndex extends React.Component {
               <h5>In my free time:</h5>
               <ul>
                 <li>Running and Working Out</li>
+                <li>Hanging out with my wife and daughter</li>
                 <li>Everything Green Bay Packers #gopackgo</li>
-                <li>Enjoy a nice beer or whiskey</li>
-                <li>Finding new music and playing guitar</li>
+                <li>Discovering new music</li>
+                <li>Reading a good book</li>
               </ul>
               <a className="btn" href="https://drive.google.com/open?id=0B7T-FgaP1kHeQXg4YUJWYVlhYms" target="_blank" rel="noopener noreferrer">Download Résumé</a>
             </ScrollAnimation>
